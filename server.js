f8010b2d92bc6652b01efb22f8fb3ee2b833511f
prinshisrivastava800-ux <prinshisrivastava800@gmail.com>
2026-07-13 00:58:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.resolve(__dirname)));
 
 // ===========================================
 // Frontend Routes
 // ===========================================
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.get("/test", (req, res) => {
